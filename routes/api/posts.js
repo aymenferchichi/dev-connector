c77f056f1ecea21489dbb449c77c7abf4cc06256
aymenferchichi <76875427+aymenferchichi@users.noreply.github.com>
2022-03-08 15:25:51 +0100

    first commit

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -144,4 +144,62 @@ router.put("/unlike/:id", auth, async (req, res) => {
     res.status(500).send("server error");
   }
 });
+
+// @route     Post api/posts/comment
+// @desc      Comment on a post
+// @ access   Private
+router.post(
+  "/comment/:id",
+  [auth, [check("text", "Text is required").not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      const user = await User.findById(req.user.id).select("_password");
+      const post = await Post.findById(req.params.id);
+      const newComment = {
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      };
+
+      post.comments.unshift(newComment);
+
+      await post.save();
+      res.json(post.comments);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("server error");
+    }
+  }
+);
+
+// @route     Delete api/posts/comment/:id/:comment_id
+// @desc      Delete a comment
+// @ access   Private
+router.delete("/comment/:id/:comment_id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // pull out the comment
+    const comment = post.comments.find(
+      (comment) => comment.id === req.params.comment_id
+    );
+    // see if the comment exists
+    if (!comment) {
+      return res.status(404).json({ msg: "Comment not found" });
+    }
+    // check user
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "user not authorized" });
+    }
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send("server error");
+  }
+});
+
 module.exports = router;
